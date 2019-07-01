78e1100ac94d764c2547179b8419981f16914711
Саша <alexwebseo777@gmail.com>
2019-07-01 11:28:46 +0300

    migration

---
+++ database/migrations/2019_07_02_081345_create_table_employees.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEmployees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama', 100);
+            $table->string('email', 100)->unique();
+            $table->string('phone', 100);
+            $table->float('salary', 10, 2);
+            $table->string('photo', 100);
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
